35564e64538164867c5619423e9006202a95b028
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-03-04 17:13:21 +0530

    Add files via upload

---
+++ Module1/4-3-Basics/OpEx2.java
@@ -0,0 +1,25 @@
+
+public class OpEx2 
+{
+	public static void main(String[] args) 
+	{
+		
+		int a = 6;
+		int b= 5;
+		
+		
+		a+=b;//a=a+b
+		System.out.println(a);//11
+		
+		a-=b;//a=a-b
+		System.out.println(a);
+		
+		a*=b;
+		System.out.println(a);
+		
+		a/=b;
+		System.out.println(a);
+		
+		
+	}
+}
